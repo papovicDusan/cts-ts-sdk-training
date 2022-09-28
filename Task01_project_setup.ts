8823d277ec898b289ac471bf813a4eb9a6a20946
dusanPapovic <dusan.papovic@vivifyideas.com>
2022-09-28 02:20:33 +0200

    feat: task 1

--- Task01_project_setup.ts
+++ Task01_project_setup.ts
@@ -8,10 +8,24 @@ import { apiRoot } from "./handson/client";
 // https://docs.commercetools.com/http-api-projects-project.html#get-project
 
 // TODO: Get project settings
-
+// apiRoot.get().execute().then(log).catch(log);
 
 // TODO: Get shipping method by id
 
+// apiRoot
+//   .shippingMethods()
+//   .withId({ ID: "3b7d4fc9-2092-4aef-b029-9d03c379fe80" })
+//   .get()
+//   .execute()
+//   .then(log)
+//   .catch(log);
 
 // TODO: Get standard tax category by key
 
+// apiRoot
+//   .taxCategories()
+//   .withKey({ key: "keyString" })
+//   .get()
+//   .execute()
+//   .then(log)
+//   .catch(log);
--- handson/client.ts
+++ handson/client.ts
@@ -1,29 +1,143 @@
 import { createApiBuilderFromCtpClient as createImportApiBuilderFromCtpClient } from "@commercetools/importapi-sdk";
 import { createApiBuilderFromCtpClient } from "@commercetools/platform-sdk";
-import { AuthMiddlewareOptions, ClientBuilder, HttpMiddlewareOptions, PasswordAuthMiddlewareOptions } from "@commercetools/sdk-client-v2";
+import {
+  AuthMiddlewareOptions,
+  ClientBuilder,
+  HttpMiddlewareOptions,
+  PasswordAuthMiddlewareOptions,
+} from "@commercetools/sdk-client-v2";
 import fetch from "node-fetch";
 import { ApiRoot, ImportApiRoot } from "../types/global";
 import { Prefix, Config, readConfig } from "../utils/config";
 
-
 const createApiClient = () => {
-    throw new Error("Function not implemented");
-}
+  const { oauthHost, clientId, clientSecret, projectKey, host }: Config =
+    readConfig(Prefix.DEV);
+
+  const authMiddlewareOptions: AuthMiddlewareOptions = {
+    host: oauthHost,
+    credentials: {
+      clientId: clientId,
+      clientSecret: clientSecret,
+    },
+    projectKey: projectKey,
+    fetch,
+  };
+
+  const httpMiddlewareOptions: HttpMiddlewareOptions = {
+    host: host,
+    fetch,
+  };
+
+  const client = new ClientBuilder()
+    .withClientCredentialsFlow(authMiddlewareOptions)
+    .withHttpMiddleware(httpMiddlewareOptions)
+    .build();
+
+  return createApiBuilderFromCtpClient(client).withProjectKey({
+    projectKey: projectKey,
+  });
+};
 
 const createImportApiClient = () => {
-    throw new Error("Function not implemented");
-}
+  const { oauthHost, clientId, clientSecret, projectKey, host }: Config =
+    readConfig(Prefix.IMPORT);
+
+  const authMiddlewareOptions: AuthMiddlewareOptions = {
+    host: oauthHost,
+    credentials: {
+      clientId: clientId,
+      clientSecret: clientSecret,
+    },
+    projectKey: projectKey,
+    fetch,
+  };
+
+  const httpMiddlewareOptions: HttpMiddlewareOptions = {
+    host: host,
+    fetch,
+  };
+
+  const client = new ClientBuilder()
+    .withClientCredentialsFlow(authMiddlewareOptions)
+    .withHttpMiddleware(httpMiddlewareOptions)
+    .build();
+
+  return createImportApiBuilderFromCtpClient(client).withProjectKeyValue({
+    projectKey: projectKey,
+  });
+};
 
 const createStoreApiClient = () => {
-    throw new Error("Function not implemented");
-}
+  const { oauthHost, clientId, clientSecret, projectKey, host }: Config =
+    readConfig(Prefix.STORE);
+
+  const authMiddlewareOptions: AuthMiddlewareOptions = {
+    host: oauthHost,
+    credentials: {
+      clientId: clientId,
+      clientSecret: clientSecret,
+    },
+    projectKey: projectKey,
+    fetch,
+  };
+
+  const httpMiddlewareOptions: HttpMiddlewareOptions = {
+    host: host,
+    fetch,
+  };
+
+  const client = new ClientBuilder()
+    .withClientCredentialsFlow(authMiddlewareOptions)
+    .withHttpMiddleware(httpMiddlewareOptions)
+    .build();
+
+  return createApiBuilderFromCtpClient(client).withProjectKey({
+    projectKey: projectKey,
+  });
+};
 
 const createMyApiClient = () => {
-    throw new Error("Function not implemented");
-}
+  const {
+    oauthHost,
+    clientId,
+    clientSecret,
+    projectKey,
+    host,
+    username,
+    password,
+  }: Config = readConfig(Prefix.ME);
 
+  const passwordAuthMiddlewareOptions: PasswordAuthMiddlewareOptions = {
+    host: oauthHost,
+    credentials: {
+      clientId: clientId,
+      clientSecret: clientSecret,
+      user: {
+        username: username,
+        password: password,
+      },
+    },
+    projectKey: projectKey,
+    fetch,
+  };
+
+  const httpMiddlewareOptions: HttpMiddlewareOptions = {
+    host: host,
+    fetch,
+  };
+
+  const client = new ClientBuilder()
+    .withPasswordFlow(passwordAuthMiddlewareOptions)
+    .withHttpMiddleware(httpMiddlewareOptions)
+    .build();
+
+  return createApiBuilderFromCtpClient(client).withProjectKey({
+    projectKey: projectKey,
+  });
+};
 
 export const apiRoot: ApiRoot = createApiClient();
-// export const importApiRoot: ImportApiRoot = createImportApiClient();
-// export const storeApiRoot: ApiRoot = createStoreApiClient();
-// export const myApiRoot: ApiRoot = createMyApiClient();
+export const importApiRoot: ImportApiRoot = createImportApiClient();
+export const storeApiRoot: ApiRoot = createStoreApiClient();
+export const myApiRoot: ApiRoot = createMyApiClient();
