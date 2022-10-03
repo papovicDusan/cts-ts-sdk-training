d24dbbad4052f5629bfb1212960dfec030703a5e
dusanPapovic <dusan.papovic@vivifyideas.com>
2022-10-03 16:41:55 +0200

    feat: task 5c

--- handson/my.ts
+++ handson/my.ts
@@ -1,13 +1,19 @@
-import { ClientResponse, Customer, OrderPagedQueryResponse } from "@commercetools/platform-sdk";
+import {
+  ClientResponse,
+  Customer,
+  OrderPagedQueryResponse,
+} from "@commercetools/platform-sdk";
 import { myApiRoot } from "./client";
 
 // TODO update client.ts file
 // TODO: SPA api-client
 
 export const getMe = (): Promise<ClientResponse<Customer>> => {
-    throw new Error("Function not implemented");
-}
+  return myApiRoot.me().get().execute();
+};
 
-export const getMyOrders = (): Promise<ClientResponse<OrderPagedQueryResponse>> => {
-    throw new Error("Function not implemented");
-}
+export const getMyOrders = (): Promise<
+  ClientResponse<OrderPagedQueryResponse>
+> => {
+  return myApiRoot.me().orders().get().execute();
+};
