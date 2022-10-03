e85e522722843a9ea9f10227b11f16a5f081efa9
dusanPapovic <dusan.papovic@vivifyideas.com>
2022-10-03 14:30:56 +0200

    feat: task 5a

--- Task05a_stores.ts
+++ Task05a_stores.ts
@@ -1,25 +1,26 @@
 import { apiRoot } from "./handson/client";
-import { getStoreByKey, getCustomersInStore, createInStoreCart } from "./handson/store";
+import {
+  getStoreByKey,
+  getCustomersInStore,
+  createInStoreCart,
+} from "./handson/store";
 import { getCustomerByKey } from "./handson/customer";
 import { log } from "./utils/logger";
 
-const storeKey = "berlin-store";
-
+// const storeKey = "berlin-store";
+const storeKey = "europe";
 // getStoreByKey(storeKey).then(log).catch(log);
 
 // getCustomersInStore(storeKey)
-//     .then(customers => {
-//         log(customers.body.count);
-//         customers.body.results.forEach(customer =>
-//             log(customer.id)
-//         )
-//     })
-//     .catch(log);
+//   .then((customers) => {
+//     log(customers.body.count);
+//     customers.body.results.forEach((customer) => log(customer.id));
+//   })
+//   .catch(log);
 
 // getCustomerByKey("tt-customer")
-//     .then(customer => {
-//         createInStoreCart(storeKey, customer)
-//             .then(log)
-//             .catch(log);
-//     })
-//     .catch(log);
+getCustomerByKey("newKey")
+  .then((customer) => {
+    createInStoreCart(storeKey, customer).then(log).catch(log);
+  })
+  .catch(log);
--- handson/store.ts
+++ handson/store.ts
@@ -1,27 +1,56 @@
-import { Cart, ClientResponse, Customer, CustomerPagedQueryResponse, ProductsInStorePagedQueryResponse, Store } from "@commercetools/platform-sdk";
+import {
+  Cart,
+  ClientResponse,
+  Customer,
+  CustomerPagedQueryResponse,
+  ProductsInStorePagedQueryResponse,
+  Store,
+} from "@commercetools/platform-sdk";
 import { apiRoot, storeApiRoot } from "./client";
 
 //TODO: update client.ts file
 
 export const getStoreByKey = (key: string): Promise<ClientResponse<Store>> => {
-    throw new Error("Function not implemented");
-}
+  return apiRoot.stores().withKey({ key: key }).get().execute();
+};
 
-export const getCustomersInStore = (storeKey: string): Promise<ClientResponse<CustomerPagedQueryResponse>> => {
-    throw new Error("Function not implemented");
-}
+export const getCustomersInStore = (
+  storeKey: string
+): Promise<ClientResponse<CustomerPagedQueryResponse>> => {
+  return apiRoot
+    .inStoreKeyWithStoreKeyValue({ storeKey: storeKey })
+    .customers()
+    .get()
+    .execute();
+};
 
-export const addProductSelectionToStore = (storeKey: string, productSelectionKey: string): Promise<ClientResponse<Store>> => {
-    throw new Error("Function not implemented");
-}
+export const addProductSelectionToStore = (
+  storeKey: string,
+  productSelectionKey: string
+): Promise<ClientResponse<Store>> => {
+  throw new Error("Function not implemented");
+};
 
-export const getProductsInStore = (storeKey: string): Promise<ClientResponse<ProductsInStorePagedQueryResponse>> => {
-    throw new Error("Function not implemented");
-}
+export const getProductsInStore = (
+  storeKey: string
+): Promise<ClientResponse<ProductsInStorePagedQueryResponse>> => {
+  throw new Error("Function not implemented");
+};
 
-export const createInStoreCart = (storeKey: string, customer: ClientResponse<Customer>): Promise<ClientResponse<Cart>> => {
-    throw new Error("Function not implemented");
-}
-
-
-
+export const createInStoreCart = (
+  storeKey: string,
+  customer: ClientResponse<Customer>
+): Promise<ClientResponse<Cart>> => {
+  return apiRoot
+    .inStoreKeyWithStoreKeyValue({ storeKey })
+    .carts()
+    .post({
+      body: {
+        customerId: customer.body.id,
+        customerEmail: customer.body.email,
+        currency: "USD",
+        country: "US",
+      },
+    })
+    .execute();
+};
