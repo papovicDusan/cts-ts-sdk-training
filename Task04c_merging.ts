6ae84cd8cd8c98483d12cca4b000a65552ebbab5
dusanPapovic <dusan.papovic@vivifyideas.com>
2022-10-03 11:50:38 +0200

    feat: task 4c

--- Task04c_merging.ts
+++ Task04c_merging.ts
@@ -2,36 +2,41 @@ import { CustomerSignin } from "@commercetools/platform-sdk";
 import * as checkout from "./handson/order";
 import { log } from "./utils/logger";
 
-const customerKey = "tt-customer";
+const customerKey = "johnDoe";
 
 const mergingProcessTest = async () => {
-    let anonymousCart = await checkout.createAnonymousCart();
+  let anonymousCart = await checkout.createAnonymousCart();
 
-    let customerCart = await checkout.createCart(customerKey);
+  let customerCart = await checkout.createCart(customerKey);
 
-    anonymousCart = await checkout.addLineItemsToCart(anonymousCart.body.id, ['tulip-seed-box', 'tulip-seed-box', 'tulip-seed-box']);
+  anonymousCart = await checkout.addLineItemsToCart(anonymousCart.body.id, [
+    "A0E200000001WG3",
+    "A0E2000000027DV",
+  ]);
 
-    customerCart = await checkout.addLineItemsToCart(customerCart.body.id, ['tulip-seed-box', 'tulip-seed-sack', 'tulip-seed-package']);
+  customerCart = await checkout.addLineItemsToCart(customerCart.body.id, [
+    "A0E200000001WG3",
+  ]);
 
-    log("Anonymous Cart: " + anonymousCart.body.id);
-    log("Customer Cart: " + customerCart.body.id);
+  log("Anonymous Cart: " + anonymousCart.body.id);
+  log("Customer Cart: " + customerCart.body.id);
 
-    const customerDetails: CustomerSignin = {
-        email: "test@test.com",
-        password: "password",
-        anonymousCartId: anonymousCart.body.id,
-        anonymousCartSignInMode: "MergeWithExistingCustomerCart", // try switching to UseAsNewActiveCustomerCart
-    };
+  const customerDetails: CustomerSignin = {
+    email: "test@test.com",
+    password: "password",
+    anonymousCartId: anonymousCart.body.id,
+    anonymousCartSignInMode: "MergeWithExistingCustomerCart", // try switching to UseAsNewActiveCustomerCart
+  };
 
-    let result = await checkout.customerSignIn(customerDetails);
-    return result.body.cart;
+  let result = await checkout.customerSignIn(customerDetails);
+  return result.body.cart;
 };
 
 mergingProcessTest()
-    .then((cart) => {
-        log("Active cart: " + cart!.id);
-        cart!.lineItems.forEach(item => {
-            log(item.variant.sku + " :" + item.quantity);
-        });
-    })
-    .catch(log);
+  .then((cart) => {
+    log("Active cart: " + cart!.id);
+    cart!.lineItems.forEach((item) => {
+      log(item.variant.sku + " :" + item.quantity);
+    });
+  })
+  .catch(log);
